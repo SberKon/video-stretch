e12916e595ef8bd28f52d31b39878ba7e8d4d465
SberKon <47828218+SberKon@users.noreply.github.com>
2026-03-11 02:37:09 -0500

    Refactor video handler function syntax

--- api/video/[id].js
+++ api/video/[id].js
@@ -1,16 +1,15 @@
 const store = global._videoStore || (global._videoStore = new Map());
 
-module.exports = function handler(req, res) {
+function handler(req, res) {
 res.setHeader(‘Access-Control-Allow-Origin’, ‘*’);
-
-const { id } = req.query;
+const id = req.query.id;
 const entry = store.get(id);
-
 if (!entry) return res.status(404).json({ error: ‘Not found or expired’ });
-
 res.setHeader(‘Content-Type’, entry.contentType);
 res.setHeader(‘Content-Length’, entry.buffer.length);
 res.setHeader(‘Content-Disposition’, ‘attachment; filename=“stretched_2732x2048.mp4”’);
 res.setHeader(‘Cache-Control’, ‘no-store’);
 return res.status(200).send(entry.buffer);
-};
+}
+
+module.exports = handler;
